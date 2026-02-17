bc00596c7bcdc2f65ba736bf1b8599f0b706d795
2175 L12 <info@fightingcalculators.org>
2026-02-16 20:27:24 -0600

    More complete Slew Rate Limiting

--- utils/slew2d.py
+++ utils/slew2d.py
@@ -20,7 +20,8 @@ class SlewRateLimiter2D(Generic[Unit]):
         # - Save that to lastOutput.
         # - Return that value to the outer code.
         # There are automated tests for this that you can run.
-        self.rValue = Vector2d(target.x-target.x, self.lastOutput.y - self.lastOutput.y)
-        self.rValue = math.sqrt(self.rValue.x**2 + self.rValue.y**2)
-        self.scaleAmount = self.rValue / self.rateLimit
-        self.lastOutput = self.lastOutput * self.scaleAmount
+        x :Vector2d = Vector2d(target.x-self.lastOutput.x, target.y - self.lastOutput.y)
+        xLength = math.sqrt(x.x**2 + x.y**2)
+        r : Vector2d = x * (self.rateLimit / xLength)
+        self.lastOutput = self.lastOutput + r
+        return self.lastOutput
